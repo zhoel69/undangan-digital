23bfbfe00bf27cf8d289f0f23368c12488ec0c80
zhoel69 <157408826+zhoel69@users.noreply.github.com>
2026-02-17 21:41:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,51 @@
+const CONFIG = {
+
+  couple: {
+    groom: {
+      fullName: "Nama Lengkap Pria",
+      shortName: "Nama Panggilan Pria",
+      father: "Bapak ...",
+      mother: "Ibu ..."
+    },
+    bride: {
+      fullName: "Nama Lengkap Wanita",
+      shortName: "Nama Panggilan Wanita",
+      father: "Bapak ...",
+      mother: "Ibu ..."
+    }
+  },
+
+  wedding: {
+    dateDisplay: "Minggu, 10 Januari 2026",
+    countdownDate: "2026-01-10T08:00:00",
+
+    akad: {
+      date: "Minggu, 10 Januari 2026",
+      time: "08.00 WIB",
+      location: "Masjid Al-Ikhlas, Jakarta",
+      maps: "https://maps.google.com"
+    },
+
+    resepsi: {
+      date: "Minggu, 10 Januari 2026",
+      time: "11.00 - 14.00 WIB",
+      location: "Gedung Serbaguna Jakarta",
+      maps: "https://maps.google.com"
+    }
+  },
+
+  rsvp: {
+    phoneNumber: "6281234567890" // tanpa +
+  },
+
+  gift: {
+    bankName: "Bank BCA",
+    accountNumber: "1234567890",
+    accountName: "Nama Pemilik Rekening"
+  },
+
+  music: {
+    autoplay: true
+  }
+
+};
